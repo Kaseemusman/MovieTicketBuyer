89d7a76102dae1366a1217ce8d836db008681fc2
Kaseemusman <149836441+Kaseemusman@users.noreply.github.com>
2025-12-07 14:53:46 -0500

    Local updates before pulling

--- MovieTicketBuyer/FormMain.Designer.cs
+++ MovieTicketBuyer/FormMain.Designer.cs
@@ -54,138 +54,95 @@
             // 
             this.dgvMovieList.AllowUserToAddRows = false;
             this.dgvMovieList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMovieList.Location = new System.Drawing.Point(362, 10);
-            this.dgvMovieList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvMovieList.Location = new System.Drawing.Point(379, 12);
+            this.dgvMovieList.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvMovieList.Name = "dgvMovieList";
             this.dgvMovieList.ReadOnly = true;
             this.dgvMovieList.RowHeadersWidth = 51;
             this.dgvMovieList.RowTemplate.Height = 24;
             this.dgvMovieList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvMovieList.Size = new System.Drawing.Size(460, 122);
+            this.dgvMovieList.Size = new System.Drawing.Size(946, 251);
             this.dgvMovieList.TabIndex = 0;
             this.dgvMovieList.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMovieList_CellDoubleClick);
-
             // 
             // cboTheater
             // 
             this.cboTheater.FormattingEnabled = true;
-            this.cboTheater.Items.AddRange(new object[] {
-            "All",
-            "AMC",
-            "Regal",
-            "Cinemark"});
-            this.cboTheater.Location = new System.Drawing.Point(113, 16);
-            this.cboTheater.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboTheater.Location = new System.Drawing.Point(151, 20);
+            this.cboTheater.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cboTheater.Name = "cboTheater";
-            this.cboTheater.Size = new System.Drawing.Size(92, 21);
+            this.cboTheater.Size = new System.Drawing.Size(121, 24);
             this.cboTheater.TabIndex = 1;
             // 
             // lblTheater
             // 
             this.lblTheater.AutoSize = true;
-            this.lblTheater.Location = new System.Drawing.Point(18, 19);
-            this.lblTheater.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTheater.Location = new System.Drawing.Point(24, 23);
             this.lblTheater.Name = "lblTheater";
-            this.lblTheater.Size = new System.Drawing.Size(83, 13);
+            this.lblTheater.Size = new System.Drawing.Size(104, 16);
             this.lblTheater.TabIndex = 2;
             this.lblTheater.Text = "Filter by Theater";
             // 
             // lblGenre
             // 
             this.lblGenre.AutoSize = true;
-            this.lblGenre.Location = new System.Drawing.Point(18, 47);
-            this.lblGenre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblGenre.Location = new System.Drawing.Point(24, 58);
             this.lblGenre.Name = "lblGenre";
-            this.lblGenre.Size = new System.Drawing.Size(75, 13);
+            this.lblGenre.Size = new System.Drawing.Size(94, 16);
             this.lblGenre.TabIndex = 3;
             this.lblGenre.Text = "Filter by Genre";
             // 
             // cboGenre
             // 
             this.cboGenre.FormattingEnabled = true;
-            this.cboGenre.Items.AddRange(new object[] {
-            "All",
-            "Sci-Fi",
-            "Drama",
-            "Animation",
-            "Adventure",
-            "Thriller",
-            "Horror"});
-            this.cboGenre.Location = new System.Drawing.Point(113, 47);
-            this.cboGenre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboGenre.Location = new System.Drawing.Point(151, 58);
+            this.cboGenre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cboGenre.Name = "cboGenre";
-            this.cboGenre.Size = new System.Drawing.Size(92, 21);
+            this.cboGenre.Size = new System.Drawing.Size(121, 24);
             this.cboGenre.TabIndex = 4;
             // 
             // lblPrice
             // 
             this.lblPrice.AutoSize = true;
-            this.lblPrice.Location = new System.Drawing.Point(18, 78);
-            this.lblPrice.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPrice.Location = new System.Drawing.Point(24, 96);
             this.lblPrice.Name = "lblPrice";
-            this.lblPrice.Size = new System.Drawing.Size(70, 13);
+            this.lblPrice.Size = new System.Drawing.Size(88, 16);
             this.lblPrice.TabIndex = 5;
             this.lblPrice.Text = "Filter by Price";
             // 
             // cboPrice
             // 
             this.cboPrice.FormattingEnabled = true;
-            this.cboPrice.Items.AddRange(new object[] {
-            "All",
-            "",
-            "",
-            "Under $10",
-            "",
-            "",
-            "$10-$15",
-            "",
-            "",
-            "Over $15"});
-            this.cboPrice.Location = new System.Drawing.Point(113, 76);
-            this.cboPrice.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboPrice.Location = new System.Drawing.Point(151, 94);
+            this.cboPrice.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cboPrice.Name = "cboPrice";
-            this.cboPrice.Size = new System.Drawing.Size(92, 21);
+            this.cboPrice.Size = new System.Drawing.Size(121, 24);
             this.cboPrice.TabIndex = 6;
             // 
             // lblTime
             // 
             this.lblTime.AutoSize = true;
-            this.lblTime.Location = new System.Drawing.Point(18, 106);
-            this.lblTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTime.Location = new System.Drawing.Point(24, 130);
             this.lblTime.Name = "lblTime";
-            this.lblTime.Size = new System.Drawing.Size(69, 13);
+            this.lblTime.Size = new System.Drawing.Size(88, 16);
             this.lblTime.TabIndex = 7;
             this.lblTime.Text = "Filter by Time";
             // 
             // cboTime
             // 
             this.cboTime.FormattingEnabled = true;
-            this.cboTime.Items.AddRange(new object[] {
-            "All",
-            "",
-            "",
-            "Morning",
-            "",
-            "",
-            "Afternoon",
-            "",
-            "",
-            "Evening",
-            "",
-            "",
-            "Night"});
-            this.cboTime.Location = new System.Drawing.Point(113, 103);
-            this.cboTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboTime.Location = new System.Drawing.Point(151, 127);
+            this.cboTime.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cboTime.Name = "cboTime";
-            this.cboTime.Size = new System.Drawing.Size(92, 21);
+            this.cboTime.Size = new System.Drawing.Size(121, 24);
             this.cboTime.TabIndex = 8;
             // 
             // btnFilter
             // 
-            this.btnFilter.Location = new System.Drawing.Point(20, 137);
-            this.btnFilter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnFilter.Location = new System.Drawing.Point(27, 169);
+            this.btnFilter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnFilter.Name = "btnFilter";
-            this.btnFilter.Size = new System.Drawing.Size(78, 26);
+            this.btnFilter.Size = new System.Drawing.Size(104, 32);
             this.btnFilter.TabIndex = 9;
             this.btnFilter.Text = "Apply Filter";
             this.btnFilter.UseVisualStyleBackColor = true;
@@ -194,28 +151,27 @@
             // lblSort
             // 
             this.lblSort.AutoSize = true;
-            this.lblSort.Location = new System.Drawing.Point(18, 228);
-            this.lblSort.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSort.Location = new System.Drawing.Point(24, 281);
             this.lblSort.Name = "lblSort";
-            this.lblSort.Size = new System.Drawing.Size(78, 13);
+            this.lblSort.Size = new System.Drawing.Size(97, 16);
             this.lblSort.TabIndex = 10;
             this.lblSort.Text = "Sort Movies By";
             // 
             // cboSort
             // 
             this.cboSort.FormattingEnabled = true;
-            this.cboSort.Location = new System.Drawing.Point(113, 226);
-            this.cboSort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboSort.Location = new System.Drawing.Point(151, 278);
+            this.cboSort.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cboSort.Name = "cboSort";
-            this.cboSort.Size = new System.Drawing.Size(92, 21);
+            this.cboSort.Size = new System.Drawing.Size(121, 24);
             this.cboSort.TabIndex = 11;
             // 
             // btnSort
             // 
-            this.btnSort.Location = new System.Drawing.Point(18, 258);
-            this.btnSort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSort.Location = new System.Drawing.Point(24, 318);
+            this.btnSort.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSort.Name = "btnSort";
-            this.btnSort.Size = new System.Drawing.Size(78, 26);
+            this.btnSort.Size = new System.Drawing.Size(104, 32);
             this.btnSort.TabIndex = 12;
             this.btnSort.Text = "Apply Sort";
             this.btnSort.UseVisualStyleBackColor = true;
@@ -223,10 +179,10 @@
             // 
             // btnAddWatchlist
             // 
-            this.btnAddWatchlist.Location = new System.Drawing.Point(620, 154);
-            this.btnAddWatchlist.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAddWatchlist.Location = new System.Drawing.Point(776, 278);
+            this.btnAddWatchlist.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAddWatchlist.Name = "btnAddWatchlist";
-            this.btnAddWatchlist.Size = new System.Drawing.Size(98, 26);
+            this.btnAddWatchlist.Size = new System.Drawing.Size(131, 32);
             this.btnAddWatchlist.TabIndex = 13;
             this.btnAddWatchlist.Text = "Add to Watchlist";
             this.btnAddWatchlist.UseVisualStyleBackColor = true;
@@ -234,10 +190,10 @@
             // 
             // btnBuy
             // 
-            this.btnBuy.Location = new System.Drawing.Point(362, 154);
-            this.btnBuy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBuy.Location = new System.Drawing.Point(379, 278);
+            this.btnBuy.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnBuy.Name = "btnBuy";
-            this.btnBuy.Size = new System.Drawing.Size(94, 26);
+            this.btnBuy.Size = new System.Drawing.Size(125, 32);
             this.btnBuy.TabIndex = 14;
             this.btnBuy.Text = "Buy Ticket";
             this.btnBuy.UseVisualStyleBackColor = true;
@@ -245,10 +201,10 @@
             // 
             // btnOpenWatchlist
             // 
-            this.btnOpenWatchlist.Location = new System.Drawing.Point(494, 154);
-            this.btnOpenWatchlist.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOpenWatchlist.Location = new System.Drawing.Point(576, 278);
+            this.btnOpenWatchlist.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnOpenWatchlist.Name = "btnOpenWatchlist";
-            this.btnOpenWatchlist.Size = new System.Drawing.Size(98, 26);
+            this.btnOpenWatchlist.Size = new System.Drawing.Size(131, 32);
             this.btnOpenWatchlist.TabIndex = 15;
             this.btnOpenWatchlist.Text = "Open Watchlist";
             this.btnOpenWatchlist.UseVisualStyleBackColor = true;
@@ -257,27 +213,26 @@
             // lblSearch
             // 
             this.lblSearch.AutoSize = true;
-            this.lblSearch.Location = new System.Drawing.Point(18, 315);
-            this.lblSearch.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSearch.Location = new System.Drawing.Point(24, 388);
             this.lblSearch.Name = "lblSearch";
-            this.lblSearch.Size = new System.Drawing.Size(73, 13);
+            this.lblSearch.Size = new System.Drawing.Size(90, 16);
             this.lblSearch.TabIndex = 16;
             this.lblSearch.Text = "Search Movie";
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(113, 310);
-            this.txtSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSearch.Location = new System.Drawing.Point(151, 382);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(92, 20);
+            this.txtSearch.Size = new System.Drawing.Size(121, 22);
             this.txtSearch.TabIndex = 17;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(20, 347);
-            this.btnSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSearch.Location = new System.Drawing.Point(27, 427);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(78, 26);
+            this.btnSearch.Size = new System.Drawing.Size(104, 32);
             this.btnSearch.TabIndex = 18;
             this.btnSearch.Text = "Search Movie";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -285,9 +240,9 @@
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(832, 555);
+            this.ClientSize = new System.Drawing.Size(1337, 683);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtSearch);
             this.Controls.Add(this.lblSearch);
@@ -307,7 +262,7 @@
             this.Controls.Add(this.lblTheater);
             this.Controls.Add(this.cboTheater);
             this.Controls.Add(this.dgvMovieList);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormMain";
             this.Text = "FormMain";
             this.Load += new System.EventHandler(this.FormMain_Load);
